9b9f754e8ef6322e780ce86cc84fcc94d5cbde44
Nova <xueshengyuan1118@hotmail.com>
2019-07-01 13:46:13 -0400

    link to db and use update to create a decrement of quantity to show customer action

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 require("dotenv").config();
+var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
@@ -8,13 +9,64 @@ var connection = mysql.createConnection({
     database : "bamazon"
 });
 console.log("Welcome to Bamazon");
-connection.query("select *from products",function(err,data){
+connection.query("select item_id, product_name, price from products",function(err,data){
     if (err) {
         console.log(err);
+    }
+    let arr = [];
+    console.log("Here are our products");
+    for (let index = 0; index < data.length; index++) {
+        console.log(data[index].item_id +"|" +data[index].product_name +"|" +data[index].price);
+        arr.push(data[index].item_id+"."+data[index].product_name);
+        console.log("-----------------------------------------------");
         
     }
-    for (let index = 0; index < data.length; index++) {
-        console.log(data[index]);
-    }
-    //connection.end();
+
+    inquirer.prompt({
+        type : "list",
+        name : "item",
+        message : "Which do u want to buy?",
+        choices : arr
+    }).then(answer=>{
+        //console.log(answer.item);
+        //get id
+        let buyId = answer.item.substring(0,answer.item.indexOf("."));
+        //console.log(buyId);
+        connection.query("select stock_quantity from products where item_id = ?",buyId,function(err,data){
+            if (err) {
+                console.log(err);
+                
+            }
+            let stock = data[0].stock_quantity;
+            inquirer.prompt({
+                type : "input",
+                name : "quantity",
+                message : "How many do u need?",
+                default : 1
+            }).then(answer=>{
+                let quantity = answer.quantity;
+                if(quantity>0){
+                    if(quantity>stock){
+                        console.log("stock not enough");
+                        
+                    }else{
+                        //cart table
+                        //products table
+                        //use bamazon;update products set stock_quantity = stock_quantity-1 where item_id = 2;
+                        connection.query("update products set stock_quantity = stock_quantity - ? where item_id = ?",[quantity,buyId],function (err,data) {
+                            if(err){
+                                console.log(err);
+                            }
+                            if (data.affectedRows == 1) {
+                                console.log("successs");
+                            }
+                        });
+                    }
+                }else{
+                    console.log("wrong input #");
+                    
+                }
+            });
+        });
+    });
 });
